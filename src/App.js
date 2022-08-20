6b2cb934f67c46447ef84d9157616424e97d84cc
rclepf <rclepf@gmail.com>
2022-08-20 13:10:56 -0300

    projeto inicial

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Form from './components/Form';
+import Header from './components/Header';
+import Resume from './components/Resume';
+import GlobalStyle from './styles/global';
+
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Resume />
+      <Form />
+      <GlobalStyle />
+    </>
+  )
+};
+
+export default App;
